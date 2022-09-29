06d6de2f7fea22664e30a349efa27367b33c90a0
aimansae <saeedaimann@gmail.com>
2022-09-29 19:27:00 +0000

    Add form and backend messages

--- run.py
+++ run.py
@@ -2,11 +2,12 @@
 
 import os
 import json
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, flash
+if os.path.exists('env.py'):
+    import env
 
 app = Flask(__name__)
-
+app.secret_key = os.environ.get('SECRET_KEY')
 
 @app.route("/")
 # same in html for
@@ -22,7 +23,7 @@ def about():
     return render_template("about.html", page_title='about', company=data)
 
 
-@app.route("/about<member_name>")
+@app.route("/about/<member_name>")
 def about_member(member_name):
     member = {}
     with open("data/company.json", "r") as json_data:
@@ -30,12 +31,15 @@ def about_member(member_name):
         for obj in data:
             if obj['url'] == member_name:
                 member = obj
-    return "<h1>" + member["name"] + "</h1>"             
+    return render_template("member.html", member=member) 
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html", page_title='contact')
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message".format(
+            request.form.get("name")))
+    return render_template("contact.html", page_title='Contact')
 
 
 @app.route("/careers")
